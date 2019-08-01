11cdcbd9826666085ff3832f13df1ec7720d53a9
Michael Irvine <michaelirvine@MacBook-Pro-2.local>
2019-07-31 20:38:49 -0700

    name generator works. need to find better word list

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,34 +1,44 @@
 import "../SASS/styles.css";
 import React from "react";
+import data from '../word-list.json';
 
-
-
-
-// console.log(data);
 
 
 class App extends React.Component {
     state = {firstWord: '', secondWord: ''};
-    firstWordArr = ['lizard','cool','dog','wowwww'];
+    firstWordArr = data;
     secondWordArr = ['studio','agency','creative','design'];
 
-
-    componentDidMount(){
+    generateName = () => {
         this.setState({
             firstWord: this.firstWordArr[Math.floor(Math.random() * this.firstWordArr.length)],
             secondWord: this.secondWordArr[Math.floor(Math.random() * this.secondWordArr.length)],
-        })
+        });
+    };
+
+    onClick = () => {
+        this.generateName();
+    };
+
+    componentDidMount(){
+        this.generateName();
     };
 
     render() {
         return (
-            <div className="container">
-                <div className="col_01">
-                    <h2>{this.state.firstWord}</h2>
+            <div className="app">
+                <div className="container">
+                    <div className="col_01">
+                        <h2>{this.state.firstWord}</h2>
+                    </div>
+                    <div className="col_02">
+                        <h2>{this.state.secondWord}</h2>
+                    </div>
                 </div>
-                <div className="col_02">
-                    <h2>{this.state.secondWord}</h2>
-                </div>
+                
+                <div className="refresh-button">
+                    <button onClick={this.onClick}>NEW NAME</button>
+                </div>            
             </div>
         )
     }
